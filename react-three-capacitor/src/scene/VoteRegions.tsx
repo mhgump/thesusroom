47842390eecf49604fe167092e174bfd0a543ecb
Michael Gump <mhgump@gmail.com>
2026-04-22 23:05:59 -0700

    Fix vote region scale

--- react-three-capacitor/src/scene/VoteRegions.tsx
+++ react-three-capacitor/src/scene/VoteRegions.tsx
@@ -15,13 +15,13 @@ export function VoteRegions({ visibleIds }: Props) {
             <meshBasicMaterial color={region.color} transparent opacity={0.35} />
           </mesh>
           <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.003, 0]}>
-            <ringGeometry args={[region.radius - 0.12, region.radius, 64]} />
+            <ringGeometry args={[region.radius * 0.9625, region.radius, 64]} />
             <meshBasicMaterial color={region.color} />
           </mesh>
           <Text
             position={[0, 0.004, 0]}
             rotation={[-Math.PI / 2, 0, 0]}
-            fontSize={1.5}
+            fontSize={region.radius * 0.8}
             color={region.color}
             anchorX="center"
             anchorY="middle"
